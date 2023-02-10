6c1581672d6c960124881e4af9ebdfee3ee4422c
Alissa Tung <alissa-tung@outlook.com>
2023-02-10 09:44:34 +0800

    rs: cargo bug

--- rs_src/xv6/xv6-kernel/src/main.rs
+++ rs_src/xv6/xv6-kernel/src/main.rs
@@ -4,7 +4,7 @@
 use core::panic::PanicInfo;
 
 #[allow(unused_imports, clippy::single_component_path_imports)]
-use xv6;
+use xv6_kernel;
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
--- user/umalloc.c
+++ user/umalloc.c
@@ -18,7 +18,6 @@ union header {
 
 typedef union header Header;
 
-static Header base;
 static Header *freep;
 
 void free(void *ap) {
@@ -39,46 +38,4 @@ void free(void *ap) {
   } else
     p->s.ptr = bp;
   freep = p;
-}
-
-static Header *morecore(uint nu) {
-  char *p;
-  Header *hp;
-
-  if (nu < 4096)
-    nu = 4096;
-  p = sbrk(nu * sizeof(Header));
-  if (p == (char *)-1)
-    return 0;
-  hp = (Header *)p;
-  hp->s.size = nu;
-  free((void *)(hp + 1));
-  return freep;
-}
-
-void *malloc(uint nbytes) {
-  Header *p, *prevp;
-  uint nunits;
-
-  nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
-  if ((prevp = freep) == 0) {
-    base.s.ptr = freep = prevp = &base;
-    base.s.size = 0;
-  }
-  for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
-    if (p->s.size >= nunits) {
-      if (p->s.size == nunits)
-        prevp->s.ptr = p->s.ptr;
-      else {
-        p->s.size -= nunits;
-        p += p->s.size;
-        p->s.size = nunits;
-      }
-      freep = prevp;
-      return (void *)(p + 1);
-    }
-    if (p == freep)
-      if ((p = morecore(nunits)) == 0)
-        return 0;
-  }
 }
