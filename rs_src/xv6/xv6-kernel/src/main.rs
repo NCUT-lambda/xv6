fcdef05bbbad6d6322a5335a3f4b6c20280b7f08
Alissa Tung <alissa-tung@outlook.com>
2023-02-10 10:10:41 +0800

    rs: init workspace

--- rs_src/xv6/xv6-kernel/src/main.rs
+++ rs_src/xv6/xv6-kernel/src/main.rs
@@ -1,12 +1,7 @@
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 
-use core::panic::PanicInfo;
+extern crate xv6_common;
 
 #[allow(unused_imports, clippy::single_component_path_imports)]
 use xv6_kernel;
-
-#[panic_handler]
-fn panic(_: &PanicInfo) -> ! {
-    loop {}
-}
