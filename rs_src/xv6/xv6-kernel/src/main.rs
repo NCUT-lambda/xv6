31cb78bbecef5889c4f3de656272ce57bce7532d
Lithaca <lithaca@mail.ncut.edu.cn>
2023-02-28 09:24:36 +0800

    temp

--- rs_src/xv6/xv6-kernel/src/main.rs
+++ rs_src/xv6/xv6-kernel/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 
-extern crate xv6_panic;
+extern crate xv6_kernel_panic;
 
 #[allow(unused_imports, clippy::single_component_path_imports)]
 use xv6_kernel;
